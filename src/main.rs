22146786460b0a083634f2a310ac0ddbcefc67ee
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2019-10-28 20:05:48 +0100

    Show only the filename column

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ fn main() {
     println!("{}", fasd_file);
     let contents = fs::read_to_string(fasd_file).unwrap();
     for line in contents.lines() {
-        println!("{}", line);
+        let mut parts = line.split('|');
+        match parts.next() {
+            Some(file) => println!("{}", file),
+            None => eprintln!("{}", "Some error occurred at parsing the .fasd file"),
+        }
     }
 
 }
