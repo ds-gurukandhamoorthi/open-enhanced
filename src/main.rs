9fd2c0bb8406c082184c6cd6d337557754f2c14b
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2019-10-28 20:23:15 +0100

    Add filter: books

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,20 @@ use std::fs;
 
 fn main() {
     let fasd_file = String::from("/home/guru/.fasd");
-    println!("{}", fasd_file);
     let contents = fs::read_to_string(fasd_file).unwrap();
     for line in contents.lines() {
         let mut parts = line.split('|');
         match parts.next() {
-            Some(file) => println!("{}", file),
+            Some(file) => {
+                if is_book(file) {
+                    println!("{}", file);
+                }
+            }
             None => eprintln!("{}", "Some error occurred at parsing the .fasd file"),
         }
     }
+}
 
+fn is_book(file: &str) -> bool {
+    file.ends_with(".pdf") || file.ends_with(".epub") || file.ends_with(".mobi")
 }
