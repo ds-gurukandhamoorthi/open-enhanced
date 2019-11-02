40a6fd1180f00df1fc187a5d1da4641bd210d04c
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2019-11-02 18:40:15 +0100

    Refactor: use `any`

--- src/main.rs
+++ src/main.rs
@@ -69,34 +69,33 @@ fn main() {
 
 }
 
+fn file_has_extension(file: &str, extensions: Vec<&str>) -> bool{
+    extensions.iter().any(|ext| file.ends_with(ext))
+}
+
 fn is_book(file: &str) -> bool {
-    file.ends_with(".pdf") || file.ends_with(".epub") || file.ends_with(".mobi")
+    let book_exts = vec! [".pdf", ".epub", ".mobi"];
+    file_has_extension(file, book_exts)
 }
 
 fn is_audio(file: &str) -> bool {
-    file.ends_with(".mp3")
-        || file.ends_with(".wav")
-        || file.ends_with(".m4a")
-        || file.ends_with(".aac")
-        || file.ends_with(".opus")
-        || file.ends_with(".webm")
+    let audio_exts = vec! [".mp3", ".wav", ".m4a", ".aac", ".opus", ".webm"];
+    file_has_extension(file, audio_exts)
 }
 
 fn is_video(file: &str) -> bool {
-    file.ends_with(".mp4")
-        || file.ends_with(".mkv")
-        || file.ends_with(".avi")
-        || file.ends_with(".wav")
-        || file.ends_with(".mpg")
-        || file.ends_with(".webm")
+    let video_exts = vec! [".mp4",".mkv", ".avi", ".wav", ".mpg", ".webm"];
+    file_has_extension(file, video_exts)
 }
 
 fn is_image(file: &str) -> bool {
-    file.ends_with(".png") || file.ends_with(".jpg") || file.ends_with(".gif")
+    let image_exts = vec! [".png", ".jpg", ".gif"];
+    file_has_extension(file, image_exts)
 }
 
 fn is_code(file: &str) -> bool {
-    file.ends_with(".R") || file.ends_with(".py")
+    let code_exts = vec! [".R", ".py"];
+    file_has_extension(file, code_exts)
 }
 
 fn file_of_filetype(file: &str, filetype: &str) -> bool {
@@ -109,3 +108,47 @@ fn file_of_filetype(file: &str, filetype: &str) -> bool {
         _ => false,
     }
 }
+
+#[test]
+fn it_processes_pdf() {
+    assert!(is_book("sample.pdf"));
+    assert!(!is_book("samplepdf"));
+}
+
+#[test]
+fn it_processes_epub() {
+    assert!(is_book("sample.epub"));
+    assert!(!is_book("samplepub"));
+}
+
+#[test]
+fn it_processes_mobi() {
+    assert!(is_book("sample.mobi"));
+    assert!(!is_book("samplemobi"));
+}
+
+#[test]
+fn it_processes_mp3() {
+    assert!(is_audio("sample.mp3"));
+    assert!(!is_audio("samplemp3"));
+}
+
+#[test]
+fn it_processes_mp4() {
+    assert!(is_video("sample.mp4"));
+    assert!(!is_video("samplemp4"));
+}
+
+#[test]
+fn it_processes_png() {
+    assert!(is_image("sample.png"));
+    assert!(!is_image("samplepng"));
+}
+
+#[test]
+fn it_processes_r_code_files() {
+    assert!(is_code("sample.R"));
+    assert!(!is_code("sampleR"));
+}
+
+//FIXME: case sensitive or not
