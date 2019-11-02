4ccc5037b5da18549f9d66f765cbc3c20dda9142
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2019-11-02 23:30:30 +0100

    Refactor: Use array instead of vec

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::path::Path;
 use std::io::{Write, BufWriter};
 use std::process::{Command, Stdio};
 use std::str;
+use std::iter::Iterator;
 
 fn main() {
     let home = env::var("HOME").unwrap();
@@ -70,33 +71,33 @@ fn main() {
 
 }
 
-fn file_has_extension(file: &str, extensions: Vec<&str>) -> bool{
+fn file_has_extension(file: &str, extensions: &[&str]) -> bool{
     extensions.iter().any(|ext| file.ends_with(ext))
 }
 
 fn is_book(file: &str) -> bool {
-    let book_exts = vec! [".pdf", ".epub", ".mobi"];
-    file_has_extension(file, book_exts)
+    let book_exts = [".pdf", ".epub", ".mobi"];
+    file_has_extension(file, &book_exts)
 }
 
 fn is_audio(file: &str) -> bool {
-    let audio_exts = vec! [".mp3", ".wav", ".m4a", ".aac", ".opus", ".webm"];
-    file_has_extension(file, audio_exts)
+    let audio_exts = [".mp3", ".wav", ".m4a", ".aac", ".opus", ".webm"];
+    file_has_extension(file, &audio_exts)
 }
 
 fn is_video(file: &str) -> bool {
-    let video_exts = vec! [".mp4",".mkv", ".avi", ".wav", ".mpg", ".webm"];
-    file_has_extension(file, video_exts)
+    let video_exts = [".mp4",".mkv", ".avi", ".wav", ".mpg", ".webm"];
+    file_has_extension(file, &video_exts)
 }
 
 fn is_image(file: &str) -> bool {
-    let image_exts = vec! [".png", ".jpg", ".gif"];
-    file_has_extension(file, image_exts)
+    let image_exts = [".png", ".jpg", ".gif"];
+    file_has_extension(file, &image_exts)
 }
 
 fn is_code(file: &str) -> bool {
-    let code_exts = vec! [".R", ".py"];
-    file_has_extension(file, code_exts)
+    let code_exts = [".R", ".py"];
+    file_has_extension(file, &code_exts)
 }
 
 fn file_of_filetype(file: &str, filetype: &str) -> bool {
