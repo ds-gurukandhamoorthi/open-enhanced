fe662b4d78941ac9115ef580988b6ff1abd44a42
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2020-02-06 08:39:14 +0100

    Refactor: genericize function parameters

--- src/main.rs
+++ src/main.rs
@@ -84,9 +84,9 @@ fn get_current_dirs_of_tmux() -> Vec<String>{
     current_folders
 }
 
-fn show_related_files_from_directories(filetype: &str, directories: HashSet<&str>, ext_process_stdin: &mut dyn Write) -> (){
+fn show_related_files_from_directories<W: Write, T: AsRef<str>>(filetype: &str, directories: impl IntoIterator<Item = T>, ext_process_stdin: &mut W) -> (){
         for dir in directories {
-            let files = fs::read_dir(dir).unwrap();
+            let files = fs::read_dir(dir.as_ref()).unwrap();
             for file in files {
                 match file {
                     Ok(file) => {
