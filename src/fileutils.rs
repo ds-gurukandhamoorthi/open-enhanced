cb0de43df5c40c58f3142b1c8daa4bdc2a398040
SWAMINATHAN Gurukandhamoorthi <ds.gurukandhamoorthi@gmail.com>
2021-12-10 12:58:47 +0100

    Refactor: Group multiple matches into one

--- src/fileutils.rs
+++ src/fileutils.rs
@@ -1,99 +1,75 @@
-use regex::Regex;
+use regex::RegexSet;
 use once_cell::sync::Lazy;
 
-static BOOK_EXTENSIONS_PATTERN : Lazy<Regex> = Lazy::new(|| {
-    Regex::new(r#"\.(pdf|epub|mobi)$"#).unwrap()
+static EXTENSIONS_PATTERN : Lazy <RegexSet> = Lazy::new(|| {
+    // CAUTION: order is important.
+    RegexSet::new(&[
+    r#"\.(pdf|epub|mobi)$"#, //book
+    r#"\.(mp3|wav|m4a|aac|opus|webm)$"#, //video
+    r#"\.(mp4|mkv|avi|wav|mpg|webm)$"#, //audio
+    r#"\.(R|py|rs|sh)$"#, //code
+    r#"\.(png|jpg|jpe?g|gif)$"#, //image
+    ]).unwrap()
 });
-static AUDIO_EXTENSIONS_PATTERN: Lazy<Regex> = Lazy::new(|| {
-    Regex::new(r#"\.(mp3|wav|m4a|aac|opus|webm)$"#).unwrap()
-});
-static VIDEO_EXTENSIONS_PATTERN: Lazy<Regex> = Lazy::new(|| {
-    Regex::new(r#"\.(mp4|mkv|avi|wav|mpg|webm)$"#).unwrap()
-});
-static IMAGE_EXTENSIONS_PATTERN : Lazy<Regex> = Lazy::new(|| {
-    Regex::new(r#"\.(png|jpg|jpe?g|gif)$"#).unwrap()
-});
-static CODE_EXTENSIONS_PATTERN: Lazy<Regex> = Lazy::new(|| {
-    Regex::new(r#"\.(R|py|rs|sh)$"#).unwrap()
-});
-
-fn is_book(file: &str) -> bool {
-    BOOK_EXTENSIONS_PATTERN.is_match(file)
-}
-
-fn is_audio(file: &str) -> bool {
-    AUDIO_EXTENSIONS_PATTERN.is_match(file)
-}
-
-fn is_video(file: &str) -> bool {
-    VIDEO_EXTENSIONS_PATTERN.is_match(file)
-}
-
-fn is_image(file: &str) -> bool {
-    IMAGE_EXTENSIONS_PATTERN.is_match(file)
-}
-
-fn is_code(file: &str) -> bool {
-    CODE_EXTENSIONS_PATTERN.is_match(file)
-}
 
 pub fn file_of_filetype(file: &str, filetype: &str) -> bool {
+    let matches =  EXTENSIONS_PATTERN.matches(file);
     match filetype {
-        "book" => is_book(file),
-        "audio" => is_audio(file),
-        "video" => is_video(file),
-        "code" => is_code(file),
-        "image" => is_image(file),
+        "book" => matches.matched(0),
+        "audio" => matches.matched(1),
+        "video" => matches.matched(2),
+        "code" => matches.matched(3),
+        "image" => matches.matched(4),
         extension => file.ends_with(extension),
     }
 }
 
 #[test]
-    fn it_processes_pdf() {
-        assert!(is_book("sample.pdf"));
-        assert!(!is_book("samplepdf"));
-    }
+fn it_processes_pdf() {
+    assert!(file_of_filetype("sample.pdf", "book"));
+    assert!(!file_of_filetype("samplepdf", "book"));
+}
 
 #[test]
-    fn it_processes_epub() {
-        assert!(is_book("sample.epub"));
-        assert!(!is_book("samplepub"));
-    }
+fn it_processes_epub() {
+    assert!(file_of_filetype("sample.epub", "book"));
+    assert!(!file_of_filetype("samplepub", "book"));
+}
 
 #[test]
-    fn it_processes_mobi() {
-        assert!(is_book("sample.mobi"));
-        assert!(!is_book("samplemobi"));
-    }
+fn it_processes_mobi() {
+    assert!(file_of_filetype("sample.mobi", "book"));
+    assert!(!file_of_filetype("samplemobi", "book"));
+}
 
 #[test]
-    fn it_processes_mp3() {
-        assert!(is_audio("sample.mp3"));
-        assert!(!is_audio("samplemp3"));
-    }
+fn it_processes_mp3() {
+    assert!(file_of_filetype("sample.mp3", "audio"));
+    assert!(!file_of_filetype("samplemp3", "audio"));
+}
 
 #[test]
-    fn it_processes_mp4() {
-        assert!(is_video("sample.mp4"));
-        assert!(!is_video("samplemp4"));
-    }
+fn it_processes_mp4() {
+    assert!(file_of_filetype("sample.mp4", "video"));
+    assert!(!file_of_filetype("samplemp4", "video"));
+}
 
 #[test]
-    fn it_processes_png() {
-        assert!(is_image("sample.png"));
-        assert!(!is_image("samplepng"));
-    }
+fn it_processes_png() {
+    assert!(file_of_filetype("sample.png", "image"));
+    assert!(!file_of_filetype("samplepng", "image"));
+}
 
 #[test]
-    fn it_processes_jpegs() {
-        assert!(is_image("sample.jpg"));
-        assert!(is_image("sample.jpeg"));
-        assert!(!is_book("samplejpg"));
-        assert!(!is_book("samplejpeg"));
-    }
+fn it_processes_jpegs() {
+    assert!(file_of_filetype("sample.jpg", "image"));
+    assert!(file_of_filetype("sample.jpeg", "image"));
+    assert!(!file_of_filetype("samplejpg", "image"));
+    assert!(!file_of_filetype("samplejpeg", "image"));
+}
 
 #[test]
-    fn it_processes_r_code_files() {
-        assert!(is_code("sample.R"));
-        assert!(!is_code("sampleR"));
-    }
+fn it_processes_r_code_files() {
+    assert!(file_of_filetype("sample.R", "code"));
+    assert!(!file_of_filetype("sampleR", "code"));
+}
